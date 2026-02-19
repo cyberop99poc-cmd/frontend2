18efad750ec04c755868c33a307973f263663e71
cyberop99poc-cmd <cyberop.99poc@gmail.com>
2026-02-19 17:00:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000
+  }
   plugins: [vue()],
   server: {
     port: 5173,
